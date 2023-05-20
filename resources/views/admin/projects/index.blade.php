9c59c1271a68c9285ef10b9e1d8cd2209617bd52
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-20 17:43:19 +0200

    feat: table in index.blade.php

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,6 +1,32 @@
 @extends('layouts/admin')
 
 @section('content')
-  
-    Prova contenuto index projects
+
+<table class="mt-5 table table-striped">
+    <thead>
+        <th>
+            Titolo
+        </th>
+        <th>
+            Contenuto
+        </th>
+        <th>
+            Slug
+        </th>
+        <th>
+            Comandi
+        </th>
+    </thead>
+
+    <tbody>
+        @foreach($projects as $project)
+        <tr>
+            <td>{{$project->title}}</td>
+            <td>{{$project->content}}</td>
+            <td>{{$project->slug}}</td>
+            <td><a href="{{route('admin.projects.show', $project->id)}}"><i class="fa-solid fa-magnifying-glass"></i></a></td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>    
 @endsection
