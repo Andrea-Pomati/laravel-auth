a2ce74da6f457559145f538a150b3e8e60fb3cc6
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-20 17:42:58 +0200

    feat: modified public function index in ProjectController

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -14,8 +14,9 @@ class ProjectController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        return view('admin.projects.index');
+    {   
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
@@ -47,7 +48,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
+        return view('admin/projects/show', compact('project'));
     }
 
     /**
