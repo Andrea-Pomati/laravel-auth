8cc050b639ad1cb332a2656a06e3a406b48beae4
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-20 16:19:14 +0200

    feat: web.php Route ProjectController and auth verified

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -31,5 +32,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+Route::resource('projects', ProjectController::class)->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
