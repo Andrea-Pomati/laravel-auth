69a357f8e92489dc9ec4621b6f786c45b3d9a9bb
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-20 16:51:17 +0200

    feat: ProjectController.php return view index

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -15,7 +15,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return('Sei nella index dei projects');
+        return view('admin.projects.index');
     }
 
     /**
