e05e12998587f349ca8c7146b9df7867bceb1be7
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-20 18:18:10 +0200

    feat: extends and section

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,11 @@
-Ecco la dashboard
+@extends('layouts/admin')
+
+@section('content')
+  <h1>Pagina di amministrazione</h1>
+
+  <hr>
+
+  <ul>
+    <li><a href="{{route('admin.projects.index')}}">Mostra tutti i progetti</a></li>
+  </ul>
+@endsection
