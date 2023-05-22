dd430696c749de4c9a86ae4d794379f76cc2068f
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-23 00:44:13 +0200

    feat: extends and section with content

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="container">
+    <form action="{{route('admin.projects.store')}}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="title">Titolo</label>
+            <input type="text" name="title" id="title" class="form-control">
+        </div>
+
+        <div class="mb-3">
+            <label for="content">Contenuto del progetto</label>
+            <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+
+        <button class="btn btn-primary" type="submit">Aggiungi</button>
+    </form>
+</div>
+
+@endsection
