54cb95944a1242c38375a9bb7f33d741183c98af
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-23 01:33:17 +0200

    feat: added a href and div

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,21 +7,28 @@
 
     <hr class="mb-4">
 
-    <h2 class="text-center">
-        {{$project->title}}
-        <br>
-        <small>
-            <pre class="fs-5">
-                ({{$project->slug}})
-            </pre>
-        </small>
-    </h2>    
-   
+    <div class="py-3">
+        <h2 class="text-center">
+            {{$project->title}}
+            <br>
+            <small>
+                <pre class="fs-5">
+                    ({{$project->slug}})
+                </pre>
+            </small>
+        </h2>  
+    </div>
+
     <hr>
 
     <p>
         {{$project->content}}
     </p>
+
+</div>
+
+<div class="d-flex justify-content-around">
+    <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary">Modifica il progetto</a>
 </div>
 
 
