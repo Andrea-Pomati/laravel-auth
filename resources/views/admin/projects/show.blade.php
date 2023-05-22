07ac351630f96c8963b12e51f5d628dff39fb99b
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-22 23:21:40 +0200

    feat: show.blade.php content

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,10 +3,27 @@
 @section('content')
 
 <div class="main pt-5">
-    <h1>{{$project->title}}</h1>    
+    <h1 class="">Visualizzazione progetto</h1>
+
+    <hr class="mb-4">
+
+    <h2 class="text-center">
+        {{$project->title}}
+        <br>
+        <small>
+            <pre class="fs-5">
+                ({{$project->slug}})
+            </pre>
+        </small>
+    </h2>    
+   
     <hr>
+
     <p>
         {{$project->content}}
     </p>
 </div>
+
+
+
 @endsection
