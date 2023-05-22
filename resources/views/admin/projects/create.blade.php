9d7c3d73cd6c31689308f9dd8744ee43934d716f
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-23 01:47:15 +0200

    feat: @error title and content

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -9,12 +9,22 @@
         @csrf
         <div class="mb-3">
             <label for="title">Titolo</label>
-            <input type="text" name="title" id="title" class="form-control">
+            <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" value="{{old('title')}}">
+            @error('title')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="content">Contenuto del progetto</label>
-            <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="content" id="content" cols="30" rows="10" class="form-control @error('content') is-invalid @enderror">{{old('content')}}</textarea>
+            @error('content')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <button class="btn btn-primary" type="submit">Aggiungi</button>
