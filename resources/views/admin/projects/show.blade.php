123710be6c35eaf1d8e55deb091a91748cfda94d
Andrea-Pomati <pomati.andrea@gmail.com>
2023-05-23 02:26:52 +0200

    feat: added button trigger modal

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -29,13 +29,39 @@
 
 <div class="d-flex justify-content-around">
     <a href="{{route('admin.projects.edit', $project)}}" class="btn btn-primary">Modifica il progetto</a>
-
-    <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class="btn btn-danger">Elimina il progetto</button>
-    </form>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Elimina
+  </button>
 </div>
+
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina il progetto</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Sei sicuro di volere eliminare il progetto "{{$project->title}}"?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+          <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-danger">Elimina il progetto</button>
+        </form>    
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
 
 
 
